a59abd4e357c177e4690f33262fb39e64ea3ccc8
pingli <liping@yolonet.io>
2022-10-24 18:22:46 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="page">
+
+  </div>
+</template>
+<style >
+  .page{
+    width: 100%;
+    height: 100px;
+    background: red;
+  }
+</style>
+
